a04ef50ff4df7c17c93bb4dfd0522bbdd0853f7d
vikash-verma-profile <er.vikashverma@yahoo.com>
2023-05-21 16:52:41 +0530

    adadd

---
+++ logical.py
@@ -0,0 +1,13 @@
+a=4
+b=3
+
+if(not(a==2)):
+    print("value is not equal to 2")
+else:
+    print("Value is equal to 2")
+
+
+if(not(a==2 or b==3)):
+    print("if is printing==True ")
+else:
+    print("else")
